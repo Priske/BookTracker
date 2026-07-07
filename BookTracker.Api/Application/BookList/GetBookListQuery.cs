7dcffd823517873d993d82cd5acf35a15f2f2fd3
Priske <eeckman_ben@hotmail.com>
2026-07-07 09:51:47 +0200

    add paging to book list

--- BookTracker.Api/Application/BookList/GetBookListQuery.cs
+++ BookTracker.Api/Application/BookList/GetBookListQuery.cs
@@ -1,25 +1,4 @@
 /*using BookTracker.Api.Storage;
-
-namespace BookTracker.Api.Application.BookList;
-
-public class GetBookListQuery(IBookRepository bookRepository)
-{
-    public async Task<IReadOnlyList<BookInfo>> Execute()
-    {
-        var books = await bookRepository.GetAllAsync();
-        var summary = books.Select(book => new BookInfo
-        {
-            Id = book.Id,
-            Title = book.Title.Value,
-            Author = book.Author.Value,
-
-        });
-        return [.. summary]; //tolist
-    }
-}
-*/
-
-using BookTracker.Api.Storage;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookTracker.Api.Application.BookList;
@@ -38,5 +17,51 @@ public class GetBookListQuery(AppDbContext dbContext)
                     Author = book.Author.Value
                 })
             .ToListAsync();
+    }
+}
+*/
+
+using BookTracker.Api.Storage;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookTracker.Api.Application.BookList;
+
+public class GetBookListQuery(AppDbContext dbContext)
+{
+    private const int DefaultPage = 1;
+    private const int DefaultPageSize = 10;
+    private const int MinPage = 1;
+    private const int MaxPageSize = 50;
+
+    public async Task<PagedResult<BookInfo>> Execute(GetBookListRequest request)
+    {
+        var page = Math.Max(1, request.Page ?? DefaultPage);
+        var pageSize = Math.Clamp(request.PageSize ?? DefaultPageSize, MinPage, MaxPageSize);
+
+        var totalItems = await dbContext.Books.CountAsync();
+
+        var books = await dbContext.Books
+            .AsNoTracking()
+            .OrderBy(book => book.Id)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .Select(book =>
+                new BookInfo
+                {
+                    Id = book.Id,
+                    Title = book.Title.Value,
+                    Author = book.Author.Value
+                })
+            .ToListAsync();
+
+        return
+            new PagedResult<BookInfo>
+            {
+                Items = books,
+                Page = page,
+                PageSize = pageSize,
+                TotalItems = totalItems,
+                TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize)
+            };
     }
 }
