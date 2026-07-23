e66eaf93d10cb63c6914184b3fa3a1154659ec70
Ben Eeckman <eeckman_ben@hotmail.com>
2026-07-23 11:18:03 +0200

    run book tracker with docker compose

--- BookTracker.Api/Program.cs
+++ BookTracker.Api/Program.cs
@@ -2,12 +2,25 @@ using BookTracker.Api.Wiring;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddApplicationServices();
+var frontendOrigin = builder.Configuration["FrontendOrigin"]
+    ?? "http://localhost:5173";
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(frontendOrigin)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173", "http://localhost:3000")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
